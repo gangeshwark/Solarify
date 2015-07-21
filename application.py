2cdb96ab4ec85a8556019cab532551b8ece8af39
Gangeshwar Krishnamurthy <gangeshwark@gmail.com>
2015-07-21 23:54:07 +0530

    Deploy

--- application.py
+++ application.py
@@ -1,33 +1,33 @@
 from flask import Flask
 
-applicaion = Flask(__name__)
+application = Flask(__name__)
 
 
-@applicaion.route('/')
+@application.route('/')
 def hello_world():
     return 'Index Page'
 
 
-@applicaion.route('/commercial')
+@application.route('/commercial')
 def commercial():
     return 'Commercial page'
 
 
-@applicaion.route('/residential')
+@application.route('/residential')
 def residential():
     return 'Residential page'
 
 
-@applicaion.route('/industrial')
+@application.route('/industrial')
 def industrial():
     return 'Industrial page'
 
 
-@applicaion.route('/calculate')
+@application.route('/calculate')
 def calculate():
     result = '0'
     return 'result'
 
 
 if __name__ == '__main__':
-    applicaion.run()
+    application.run()
